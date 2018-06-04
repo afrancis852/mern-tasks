c2f9f5e57c4936001d1aa74036c44c06d84adaac
afrancis <albert.francis852@gmail.com>
2018-06-04 19:59:15 -0300

    Agregando react al Proyecto

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-console.log('hello world!');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class MyApp extends Component {
+
+    render() {
+        return (
+            <div>Hello world from react!!</div>
+        );
+    }
+}
+
+ReactDOM.render(<MyApp />, document.getElementById('my-app'));
